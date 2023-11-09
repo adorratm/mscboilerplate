0b9757cb87923dc055ade8ab5ba2bc86a259d167
Emre KILIÇ <emrekilic19983@gmail.com>
2023-11-09 23:28:45 +0300

    event bus added

--- comments/index.js
+++ comments/index.js
@@ -1,34 +1,45 @@
 const express = require('express');
+const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
-
-const {randomBytes} = require('crypto');
-
-app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+express.urlencoded({ extended: true });
+app.use(cors());
 
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-
-    res.send(commentsByPostId[req.params.id] || []);
-
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
-    const commentId = randomBytes(4).toString('hex');
+app.post('/posts/:id/comments', async (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
 
-    const {content} = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
 
-    const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
 
-    comments.push({id: commentId, content});
+  commentsByPostId[req.params.id] = comments;
 
-    commentsByPostId[req.params.id] = comments;
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
+    }
+  });
 
-    res.status(201).send(comments);
+  res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Received Event', req.body.type);
+
+  res.send({});
 });
 
 app.listen(4001, () => {
